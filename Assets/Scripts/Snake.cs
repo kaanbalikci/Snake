24857531380a83c9ca239b36be488ece60303bd9
kaanbalikci <kaanbalikcii@gmail.com>
2022-05-27 19:17:42 +0300

    First Commit

---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+   
+    private Vector2 rotate = Vector2.right;
+    public float speed = 1f;
+    void Start()
+    {
+        
+        StartCoroutine(Move());
+    }
+
+    void Update()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rotate = Vector2.up;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            rotate = Vector2.down;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            rotate = Vector2.left;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            rotate = Vector2.right;
+        }
+    }
+
+    /*private void FixedUpdate()
+    {
+        
+        this.transform.position = new Vector3(
+            this.transform.position.x + rotate.x,
+            this.transform.position.y + rotate.y,
+            0.0f
+            );
+    }*/
+
+    private IEnumerator Move()
+    {
+        while (true)
+        {
+            var position = transform.position;
+            position += (Vector3)rotate;
+            transform.position = position;
+
+            yield return new WaitForSeconds(speed);
+        }
+        
+
+    }
+}
